9c593501c4bd4c74c370ac62e0c2e53757d63aef
ygy <2249312343@qq.com>
2020-11-09 20:10:28 +0800

    饼图左右切换事件

--- src/components/hotproduct.vue
+++ src/components/hotproduct.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container">
-    <button>&gt;</button>
-    <button>&lt;</button>
+    <button class="left" @click="toggleItem(-1)">
+      <i class="iconfont icon-arrow-lift"></i>
+    </button>
+    <button class="right" @click="toggleItem(1)">
+      <i class="iconfont icon-arrow-right"></i>
+    </button>
     <div ref="hotRef" class="hot"></div>
   </div>
 </template>
@@ -19,6 +23,8 @@ export default {
   mounted() {
     this.initChart();
     this.getData();
+    window.addEventListener("resize", this.screenAdapter);
+    this.screenAdapter();
   },
   methods: {
     initChart() {
@@ -35,17 +41,18 @@ export default {
     },
     async getData() {
       this.dataList = await getHot();
-      console.log(this.dataList);
+      // console.log(this.dataList);
       this.updataChart();
     },
     updataChart() {
       let percent = [];
-      let seriesArr = this.dataList[0].children.map((item) => {
-        console.log(item);
+      let seriesArr = this.dataList[this.currentIndex].children.map((item) => {
+        // console.log(item);
         item.children.forEach((item) => {});
         return {
           name: item.name,
           value: item.value,
+          children: item.children,
         };
       });
       console.log(seriesArr);
@@ -55,7 +62,22 @@ export default {
         },
         tooltip: {
           formatter(arg) {
-            console.log(arg);
+            let arr = arg.data.children;
+            let str = ``;
+            let allNum = 0;
+            arr.forEach((item) => {
+              allNum += item.value;
+            });
+            // console.log(allNum, "xxxxxxx");
+            arr.forEach((item) => {
+              // console.log("xx", item.value, allNum, item.value / allNum);
+              str +=
+                item.name +
+                ":" +
+                ((item.value / allNum) * 100).toFixed(2) +
+                "% <br>";
+            });
+            return str;
           },
         },
         series: [
@@ -67,7 +89,28 @@ export default {
       };
       this.chart.setOption(updataOption);
     },
-    screenAdapter() {},
+    screenAdapter() {
+      let size = (this.$refs.hotRef.offsetWidth / 100) * 3.6;
+
+      let adapterOption = {
+        title: {
+          textStyle: {
+            fontSize: size / 2,
+          },
+        },
+      };
+      this.chart.setOption(adapterOption);
+      this.chart.resize();
+    },
+    toggleItem(num) {
+      this.currentIndex += num;
+      if (this.currentIndex == 3) {
+        this.currentIndex = 0;
+      } else if (this.currentIndex == -1) {
+        this.currentIndex = 2;
+      }
+      this.updataChart();
+    },
   },
 };
 </script>
@@ -80,6 +123,19 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  button {
+    position: absolute;
+    z-index: 100;
+    transform: translate(-50%, -50%);
+    &.left {
+      left: 5%;
+      top: 50%;
+    }
+    &.right {
+      right: 5%;
+      top: 50%;
+    }
+  }
   .hot {
     width: 95vw;
     height: 95vh;
